f163913df3f8ee6860d77821192fd415697ce156
Andrea <andrea.sciannamea@gmail.com>
2019-04-02 17:46:48 +0200

    add Ammo.java

---
+++ src/main/java/Ammo.java
@@ -0,0 +1,12 @@
+public class Ammo {
+    private String value;
+    private int id;
+
+    public String getValue() {
+        return value;
+    }
+
+    public int getId(){
+        return id;
+    }
+}
