d746628225e07ea8fc0ab02563638e7cdd5e55b3
Andrea <andrea.sciannamea@gmail.com>
2019-04-14 19:24:37 +0200

    add map test

--- src/test/java/Server/Model/MapTest.java
+++ src/test/java/Server/Model/MapTest.java
@@ -1,21 +1,33 @@
 package Server.Model;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MapTest {
 
-    @Test
-    void getSquare() {
+    private static Map map;
+
+    @BeforeAll
+    static void initMap(){
+        map = new Map(1,3,4);
     }
 
     @Test
-    void getValidSquares() {
+    void testGetValidSquares() {
+        ArrayList<Integer> validSquares = new ArrayList<>();
+        validSquares = map.getValidSquares(0,2);
+        ArrayList<Integer> tempList = new ArrayList<Integer>(Arrays.asList(0, 1, 4, 2, 5));
+        assertTrue(validSquares.equals(tempList));
     }
 
     @Test
-    void isWall() {
+    void testIsWall() {
 
     }
 }
