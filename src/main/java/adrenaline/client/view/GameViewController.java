5e98556e46b93c5038e40b74e4a4d466f55e94ed
Andrea <andrea.sciannamea@gmail.com>
2019-06-02 21:11:12 +0200

    create game view

---
+++ src/main/java/adrenaline/client/view/GameViewController.java
@@ -0,0 +1,40 @@
+package adrenaline.client.view;
+
+import adrenaline.client.controller.GameController;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+
+public class GameViewController implements ViewInterface {
+
+    public BorderPane borderPane;
+    public GridPane map;
+
+    public void setDestination(){
+
+    }
+
+    @Override
+    public void showError(String error) {
+
+    }
+
+    @Override
+    public void changeStage() {
+
+    }
+
+    @Override
+    public void setGameController(GameController gameController) {
+
+    }
+
+    @Override
+    public void notifyView() {
+
+    }
+
+    @Override
+    public void notifyTimer(Integer duration) {
+
+    }
+}
